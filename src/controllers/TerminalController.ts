e5ecf186ffa800f47ca26c61089fb67eea5f538f
felipe-b-freire <felipe.bergesch.freire@outlook.com>
2026-08-28 21:08:49 -0300

    feat: implementa menu interativo no terminal com readline

--- src/controllers/TerminalController.ts
+++ src/controllers/TerminalController.ts
@@ -1,27 +1,40 @@
 import { buscarPokemon } from "../services/PokeApiService.js";
 import { CatalogoPokemon } from "../models/CatalogoPokemon.js";
 import { lerCatalogo } from "../services/BoxService.js";
+import { createInterface } from "node:readline/promises";
 
 export async function executar(): Promise<void>{
-
+    const rl = createInterface({ input: process.stdin, output: process.stdout });
+    
     const pokemonsSalvos = await lerCatalogo();
     const catalogo = new CatalogoPokemon(pokemonsSalvos)
 
-const pikachu = await buscarPokemon("pikachu")
-if(pikachu !== null){
-   await catalogo.adicionar(pikachu)   
+    while(true){
+        console.log("1 - Buscar e adicionar Pokemon");
+        console.log("2 - Listar catalogo");
+        console.log("3 - Remover Pokemon");
+        console.log("4 - Sair");
+        const opcao = await rl.question("Escolha uma opcao: ");
+        if(opcao === "4"){
+            rl.close();
+            break
+        }
+        else if(opcao === "2"){
+            catalogo.listar();
+        }
+        else if (opcao === "1"){
+            const opcao1 = await rl.question("Digite o ID ou nome do Pokemon: ")
+            const pokemonEncontrado = await buscarPokemon(opcao1)
+            if(pokemonEncontrado != null){
+                await catalogo.adicionar(pokemonEncontrado)
+            }
+        }
+        else if(opcao === "3"){
+            const opcao3 = await rl.question("Digite o ID do pokemon: ")
+            const idNumero = Number(opcao3)
+            await catalogo.remover(idNumero)
+        }
+    }
 }
-const charmander = await buscarPokemon("charmander")
-if(charmander !== null){
-   await catalogo.adicionar(charmander)
-}
-const pikachu2 = await buscarPokemon("pikachu")
-if(pikachu2 !== null){
-   await catalogo.adicionar(pikachu2)   
-}
-await buscarPokemon("alalal")
 
-catalogo.listar();
-await catalogo.remover(25);
-catalogo.listar();
-}
+
